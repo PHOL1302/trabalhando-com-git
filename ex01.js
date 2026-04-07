98d7166cfc1602bdd802a83b0755d313442288c6
PHOL1302 <pedrolopes13022010@gmail.com>
2026-04-07 15:20:12 -0300

    feat:adicionei ex01

---
+++ ex01.js
@@ -0,0 +1,15 @@
+// const redline = require ('readline-sync')
+
+// let nome =  readline.questiom("digite o seu nome:")
+// console.log(`seu nome é: ${nome}`)
+
+// const redline = require ('readline-sync')
+
+//  let nome =  redline.question ("informe seu nome: ")
+//  let curso = readline.question("Informe seu nome: ")
+// let idade = readline.questionInt("Indorme sua idade: ")
+
+// console.log("======= Sistema DE Cadastro =======")
+// console.log(`Nome do usuario: ${nome}`)
+// console.log(`Curso do usuário: ${curso}`)
+// console.log(`ìdade do usuário: ${idade}`)
