293ef091a159843045ca617f7a73d4717a4060b1
PHOL1302 <pedrolopes13022010@gmail.com>
2026-04-07 16:11:37 -0300

    feat:subindo ex02.js

---
+++ ex02.js
@@ -0,0 +1,10 @@
+// const readline = require ('readiline-sync')
+
+// let nome = readline.question("informe seu nome")
+// let nomeDoPrato = readline.question("Informe o nome do prato: ")
+// let valorDoPrato =  readline.questionFloat("Informe o valor do prato: ")
+
+// console.log ("============ Resumo Do Pedido ===========")
+// console.log (`Cliente: $ {nome}`)
+// console.log(`Prato escolhido: $ {nomeDoPrato}`)
+// console.log(`Valor: $ {valorDoPrato}`)
