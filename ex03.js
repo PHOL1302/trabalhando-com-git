5ac5d947c2fb23ce217ada2f639cf1e3f6dafe13
PHOL1302 <pedrolopes13022010@gmail.com>
2026-04-14 09:40:18 -0300

    tirei a barra

--- ex03.js
+++ ex03.js
@@ -1,11 +1,11 @@
-// const readline = require ('readline-sync')
+const readline = require ('readline-sync')
 
-//  let nome = readline.question("informe seu nome: ")
-//  let jogoFavorito = readline.question("Informe seu jogo favorito: ")
-//  let pontuacaoAtual = readline.questionFloat("Pontuação total: ")
+ let nome = readline.question("informe seu nome: ")
+ let jogoFavorito = readline.question("Informe seu jogo favorito: ")
+ let pontuacaoAtual = readline.questionFloat("Pontuação total: ")
 
-// console.log("======= RESUMO DO PLACAR =======")
-// console.log(`Nome do usuario: ${nome}`)
-// console.log(`jogoFavorito: ${jogoFavorito}`)
-// console.log(`pontuacaoAtual: ${pontuacaoAtual}`)
+console.log("======= RESUMO DO PLACAR =======")
+console.log(`Nome do usuario: ${nome}`)
+console.log(`jogoFavorito: ${jogoFavorito}`)
+console.log(`pontuacaoAtual: ${pontuacaoAtual}`)
 
