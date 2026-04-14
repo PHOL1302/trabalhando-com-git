39d7e63456460aa85e901443f01026bc54bcb9fb
PHOL1302 <pedrolopes13022010@gmail.com>
2026-04-14 09:41:21 -0300

    tirei a barra

--- ex06.js
+++ ex06.js
@@ -1,13 +1,13 @@
-// const readline = require("readline-sync")
+const readline = require("readline-sync")
 
-// let objetivo = 40
-// let horasEstudadas = rediline.questionflot('informe quantas horas estudadas:')
-// let diferenca = horasEstudadas - objetivo
+let objetivo = 40
+let horasEstudadas = rediline.questionflot('informe quantas horas estudadas:')
+let diferenca = horasEstudadas - objetivo
 
-// console.log("====== Resultado ======")
-// if (horasEstudadas >= objetivo) {
-//     console.log (" você conseguiu estudar")
-//     console.log(`você estudou mais que o esperado! ${diferenca} horas a mais.` )
-// }else{
-//     console.log(`você não conseguiu seu hub" faltaram ${diferenca} passos`)
-// }
+console.log("====== Resultado ======")
+if (horasEstudadas >= objetivo) {
+    console.log (" você conseguiu estudar")
+    console.log(`você estudou mais que o esperado! ${diferenca} horas a mais.` )
+}else{
+    console.log(`você não conseguiu seu hub" faltaram ${diferenca} passos`)
+}
