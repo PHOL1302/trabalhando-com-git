cfd414876f29e9689d5804b0525c889eb6453d09
PHOL1302 <pedrolopes13022010@gmail.com>
2026-04-14 09:41:34 -0300

    tirei a barra

--- ex07.js
+++ ex07.js
@@ -1,11 +1,11 @@
-// const readline = require("readline-sync")
+const readline = require("readline-sync")
 
-// let nota = readline.questionFloat('informe sua nota: ')
+let nota = readline.questionFloat('informe sua nota: ')
 
-// console.log("======= Boletim do aluno =======")
+console.log("======= Boletim do aluno =======")
  
-// if(nota >= 7){
-//     console.log("Aprovado")
-// }else{
-//     console.log("você é um hubzão")
-// }
+if(nota >= 7){
+    console.log("Aprovado")
+}else{
+    console.log("você é um hubzão")
+}
